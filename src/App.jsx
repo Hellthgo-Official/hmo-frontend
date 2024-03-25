d5a3d712e1de9bf0f464eb27fbb1a2b9cd1ac437
Elijah Edun <edunelijah18@gmail.com>
2024-03-25 04:19:42 +0100

    feat: Complete signin screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PlanSelection from './pages/PlanSelection';
 import Signup from './pages/auth/Signup';
+import Signin from './pages/auth/Signin';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<PlanSelection />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
         </Routes>
       </div>
     </Router>
