6867519bf4d9c2861c2b94888b127dab40dc1a0e
inspi-writer001 <conservedinnovation@gmail.com>
2024-04-03 12:25:26 +0100

    fixed login

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = 'https://healthgo-hmo.onrender.com/api/v2';
-const BASE_URL = "http://localhost:9000/api/v2";
+const BASE_URL = "https://healthgo-hmo.onrender.com/api/v2";
+// const BASE_URL = "https://localhost:9000/api/v2";
 
 export const authApi = axios.create({
   baseURL: BASE_URL
