b1fdceee6ef320574764f8f8fcf9fe6f80518ae6
Elijah Edun <edunelijah18@gmail.com>
2024-05-08 21:40:44 +0100

    fix: handle auth notification and signup form restructure

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -7,6 +7,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { signupUserFn } from '../../api/auth';
 import useAuthStore from '../../store/auth';
+import arrowBack from '../../assets/images/arrow-back.png'
+
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -21,6 +23,9 @@ const Signup = () => {
     confirmPassword: '',
   };
 
+  const [firstForm, setFirstForm] = useState(true);
+  const [secondForm, setSecondForm] = useState(false);
+
   const [formData, setFormData] = useState(initialFormData);
 
   const [showPassword, setShowPassword] = useState(false);
@@ -31,6 +36,9 @@ const Signup = () => {
 
   const passwordIcon = showPassword ? eye : eyeClosed;
   const confirmPasswordIcon = showConfirmPassword ? eye : eyeClosed;
+
+  const [message, setMessage] = useState('');
+  const [type, setType] = useState('');
 
   const handlePswInputFocus = () => {
     setPswInputFocus(true);
@@ -52,26 +60,48 @@ const Signup = () => {
     setFormData({ ...formData, [key]: value });
   };
 
-  const storeData = useAuthStore((state) => state.setRegisterData);
-
-  const signupMutation = useMutation({
-    mutationFn: signupUserFn,
-    onSuccess: (data) => {
-      storeData(data);
-      setFormData(initialFormData);
-      navigate('/verify-email');
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-
   const [username, setUsername] = useState('');
 
   const handleUsernameChange = (event) => {
     const input = event.target.value.replace(/[^a-zA-Z0-9]/g, ''); // Allow only alphanumeric characters
     setUsername(input.slice(0, 12)); // Limit to maximum 12 characters
   };
+
+  const firstFormCheck =
+    formData.email &&
+    formData.firstName &&
+    formData.lastName &&
+    username;
+
+  const showSecondForm = (e) => {
+    e.preventDefault();
+    setFirstForm(false);
+    setSecondForm(true);
+  };
+
+  const showFirstForm = (e) => {
+    e.preventDefault();
+    setFirstForm(true);
+    setSecondForm(false);
+  };
+
+  const storeData = useAuthStore((state) => state.setRegisterData);
+
+  const signupMutation = useMutation({
+    mutationFn: signupUserFn,
+    onSuccess: (data) => {
+      setType('success');
+      setMessage('Signup successful');
+      storeData(data);
+      setFormData(initialFormData);
+      navigate('/verify-email');
+    },
+    onError: (error) => {
+      // console.log(error);
+      setType('error');
+      setMessage(error);
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -82,6 +112,8 @@ const Signup = () => {
 
     signupMutation.mutate(mainData);
   };
+
+  // console.log(signupMutation);
 
   return (
     <div className="bg-sectionBg">
@@ -104,157 +136,194 @@ const Signup = () => {
 
           <div className="w-full md:w-[50%] shadow-authFormContainer p-[30px] rounded-[1.25rem] md:rounded-[0.75rem]">
             <form className="mt-[30px] flex flex-col gap-[20px]">
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  className="auth-input"
-                  value={formData.email}
-                  onChange={(e) => handleInputChange('email', e.target.value)}
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  className="auth-input"
-                  value={formData.firstName}
-                  onChange={(e) =>
-                    handleInputChange('firstName', e.target.value)
-                  }
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                  type="text"
-                  id="lastName"
-                  className="auth-input"
-                  value={formData.lastName}
-                  onChange={(e) =>
-                    handleInputChange('lastName', e.target.value)
-                  }
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  className="auth-input"
-                  value={username}
-                  onChange={handleUsernameChange}
-                  maxLength={12} // Limit input to 12 characters
-                  pattern="[a-zA-Z0-9]+" // Allow only alphanumeric characters
-                  title="Username must be alphanumeric and maximum 12 characters"
-                  required
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input
-                  type="tel"
-                  id="phoneNumber"
-                  className="auth-input"
-                  value={formData.phoneNumber}
-                  onChange={(e) =>
-                    handleInputChange('phoneNumber', e.target.value)
-                  }
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
+              {message && (
                 <div
-                  className={`flex flex-row justify-between items-center ${
-                    pswInputFocus ? 'border-[2px]' : 'border'
-                  } ${
-                    pswInputFocus ? 'border-green-300' : 'border-green-200'
-                  }  bg-green-100 rounded-[8px] h-[3rem] w-full px-[0.5rem]`}
+                  className={`px-2 py-[10px] text-[14px] rounded-[8px] ${
+                    type === 'error'
+                      ? 'bg-red-200 text-red-600'
+                      : 'bg-green-200 text-green-600'
+                  }`}
                 >
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    id="password"
-                    className="auth-psw-input"
-                    value={formData.password}
-                    onChange={(e) =>
-                      handleInputChange('password', e.target.value)
-                    }
-                    onFocus={handlePswInputFocus}
-                    onBlur={() => setPswInputFocus(false)}
-                  />
-                  <img
-                    src={passwordIcon}
-                    onClick={toggleShowPassword}
-                    className="w-[25px] h-[24px] cursor-pointer"
-                    alt="eye"
-                  />
+                  {message}
                 </div>
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <div
-                  className={`flex flex-row justify-between items-center ${
-                    confirmPswInputFocus ? 'border-[2px]' : 'border'
-                  } ${
-                    confirmPswInputFocus
-                      ? 'border-green-300'
-                      : 'border-green-200'
-                  }  bg-green-100 rounded-[8px] h-[3rem] w-full px-[0.5rem]`}
-                >
-                  <input
-                    type={showConfirmPassword ? 'text' : 'password'}
-                    id="confirmPassword"
-                    className="auth-psw-input"
-                    value={formData.confirmPassword}
-                    onChange={(e) =>
-                      handleInputChange('confirmPassword', e.target.value)
-                    }
-                    onFocus={handleConfirmPswInputFocus}
-                    onBlur={() => setPswInputFocus(false)}
-                  />
-                  <img
-                    src={confirmPasswordIcon}
-                    onClick={toggleShowConfirmPassword}
-                    className="w-[25px] h-[24px] cursor-pointer"
-                    alt="eye"
-                  />
-                </div>
-              </div>
-
-              {signupMutation.isError && (
-                <p className="text-red-600 text-sm">
-                  {signupMutation.error.message}
-                </p>
               )}
 
-              <div className="mt-[30px] flex flex-col gap-[20px]">
-                <button
-                  type="submit"
-                  onClick={handleSubmit}
-                  disabled={signupMutation.isPending}
-                  className="bg-secondary w-full font-barlow font-semibold text-[14px] leading-[21px] tracking-[2%} text-white px-[1rem] py-[0.5rem] rounded-[4px] flex justify-center items-center gap-[8px] h-[3rem]"
-                >
-                  {signupMutation.isPending ? (
-                    <>Loading...</>
-                  ) : (
-                    <>
-                      Create Account <img src={arrowRight} alt="arrow-right" />
-                    </>
-                  )}
-                </button>
-                <Link to="/signin">
-                  <button className="bg-transparent border border-secondary w-full font-barlow font-semibold px-[1rem] py-[0.5rem] h-[48px] rounded-[4px]">
-                    Log in to an existing account
+              {
+                secondForm && (
+                  <div className="flex items-center gap-[5px] mb-[20px] cursor-pointer" onClick={showFirstForm}>
+                    <img src={arrowBack} className='w-[20px] h-[20px]' alt="arrow-back" />
+                    <p>Back</p>
+                  </div>
+                )
+              }
+
+              <div className={`${firstForm ? 'block' : 'hidden'}`}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="auth-input"
+                    value={formData.email}
+                    onChange={(e) => handleInputChange('email', e.target.value)}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    className="auth-input"
+                    value={formData.firstName}
+                    onChange={(e) =>
+                      handleInputChange('firstName', e.target.value)
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    className="auth-input"
+                    value={formData.lastName}
+                    onChange={(e) =>
+                      handleInputChange('lastName', e.target.value)
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="auth-input"
+                    value={username}
+                    onChange={handleUsernameChange}
+                    maxLength={12} // Limit input to 12 characters
+                    pattern="[a-zA-Z0-9]+" // Allow only alphanumeric characters
+                    title="Username must be alphanumeric and maximum 12 characters"
+                    required
+                  />
+                </div>
+
+                <div className="mt-[30px] flex flex-col gap-[20px]">
+                  <button
+                    type="submit"
+                    onClick={showSecondForm}
+                    disabled={!firstFormCheck}
+                    className="bg-secondary w-full font-barlow font-semibold text-[14px] leading-[21px] tracking-[2%} text-white px-[1rem] py-[0.5rem] rounded-[4px] flex justify-center items-center gap-[8px] h-[3rem]"
+                  >
+                    Proceed
                   </button>
-                </Link>
+                </div>
+              </div>
+
+              <div className={`${secondForm ? 'block' : 'hidden'}`}>
+                <div className="form-group">
+                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <input
+                    type="tel"
+                    id="phoneNumber"
+                    className="auth-input"
+                    value={formData.phoneNumber}
+                    onChange={(e) =>
+                      handleInputChange('phoneNumber', e.target.value)
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <div
+                    className={`flex flex-row justify-between items-center ${
+                      pswInputFocus ? 'border-[2px]' : 'border'
+                    } ${
+                      pswInputFocus ? 'border-green-300' : 'border-green-200'
+                    }  bg-green-100 rounded-[8px] h-[3rem] w-full px-[0.5rem]`}
+                  >
+                    <input
+                      type={showPassword ? 'text' : 'password'}
+                      id="password"
+                      className="auth-psw-input"
+                      value={formData.password}
+                      onChange={(e) =>
+                        handleInputChange('password', e.target.value)
+                      }
+                      onFocus={handlePswInputFocus}
+                      onBlur={() => setPswInputFocus(false)}
+                    />
+                    <img
+                      src={passwordIcon}
+                      onClick={toggleShowPassword}
+                      className="w-[25px] h-[24px] cursor-pointer"
+                      alt="eye"
+                    />
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <div
+                    className={`flex flex-row justify-between items-center ${
+                      confirmPswInputFocus ? 'border-[2px]' : 'border'
+                    } ${
+                      confirmPswInputFocus
+                        ? 'border-green-300'
+                        : 'border-green-200'
+                    }  bg-green-100 rounded-[8px] h-[3rem] w-full px-[0.5rem]`}
+                  >
+                    <input
+                      type={showConfirmPassword ? 'text' : 'password'}
+                      id="confirmPassword"
+                      className="auth-psw-input"
+                      value={formData.confirmPassword}
+                      onChange={(e) =>
+                        handleInputChange('confirmPassword', e.target.value)
+                      }
+                      onFocus={handleConfirmPswInputFocus}
+                      onBlur={() => setPswInputFocus(false)}
+                    />
+                    <img
+                      src={confirmPasswordIcon}
+                      onClick={toggleShowConfirmPassword}
+                      className="w-[25px] h-[24px] cursor-pointer"
+                      alt="eye"
+                    />
+                  </div>
+                </div>
+
+                {signupMutation.isError && (
+                  <p className="text-red-600 text-sm">
+                    {signupMutation.error.message}
+                  </p>
+                )}
+
+                <div className="mt-[30px] flex flex-col gap-[20px]">
+                  <button
+                    type="submit"
+                    onClick={handleSubmit}
+                    disabled={signupMutation.isPending}
+                    className="bg-secondary w-full font-barlow font-semibold text-[14px] leading-[21px] tracking-[2%} text-white px-[1rem] py-[0.5rem] rounded-[4px] flex justify-center items-center gap-[8px] h-[3rem]"
+                  >
+                    {signupMutation.isPending ? (
+                      <>Loading...</>
+                    ) : (
+                      <>
+                        Create Account{' '}
+                        <img src={arrowRight} alt="arrow-right" />
+                      </>
+                    )}
+                  </button>
+                  <Link to="/signin">
+                    <button className="bg-transparent border border-secondary w-full font-barlow font-semibold px-[1rem] py-[0.5rem] h-[48px] rounded-[4px]">
+                      Log in to an existing account
+                    </button>
+                  </Link>
+                </div>
               </div>
             </form>
           </div>
